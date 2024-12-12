86b54eefd8edabe0dcec5c48189400b34de7fb72
Joshua Parker <joshua@joshuaparker.dev>
2024-12-12 08:19:22 -0800

    First commit.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

---
+++ AdminBarPlugin.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plugin\AdminBar;
+
+use App\Infrastructure\Services\Plugin;
+use App\Shared\Services\Registry;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\SimpleCache\InvalidArgumentException;
+use Qubus\EventDispatcher\ActionFilter\Action;
+use Qubus\Exception\Exception;
+use Qubus\View\Native\Exception\InvalidTemplateNameException;
+use Qubus\View\Native\Exception\ViewException;
+use ReflectionException;
+
+use function App\Shared\Helpers\cms_enqueue_css;
+use function App\Shared\Helpers\is_user_logged_in;
+use function App\Shared\Helpers\load_plugin_textdomain;
+use function App\Shared\Helpers\plugin_basename;
+use function App\Shared\Helpers\plugin_dir_path;
+use function App\Shared\Helpers\plugin_url;
+use function dirname;
+use function get_class;
+use function Qubus\Security\Helpers\esc_html__;
+
+final class AdminBarPlugin extends Plugin
+{
+    /**
+     * @inheritDoc
+     * @throws ReflectionException|Exception
+     */
+    public function meta(): array
+    {
+        $plugin = [
+            'name' => esc_html__(string: 'AdminBar', domain: 'adminbar'),
+            'id' => 'adminbar',
+            'author' => 'Joshua Parker',
+            'version' => '1.0.0',
+            'description' => 'Adds an admin bar to Devflow site.',
+            'basename' => plugin_basename(dirname(__FILE__)),
+            'path' => plugin_dir_path(dirname(__FILE__)),
+            'url' => plugin_url('', __CLASS__),
+            'pluginUri' => 'https://github.com/getdevflow/AdminBar',
+            'authorUri' => 'https://nomadicjosh.com/',
+            'className' => get_class($this),
+        ];
+
+        Registry::getInstance()->set('adminbar', $plugin);
+
+        return $plugin;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws ContainerExceptionInterface
+     * @throws Exception
+     * @throws InvalidArgumentException
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
+     */
+    public function handle(): void
+    {
+        if (!is_user_logged_in()) {
+            return;
+        }
+        Action::getInstance()->addAction('init', [$this, 'setLocale'], 5);
+        Action::getInstance()->addAction('init', [$this, 'render'], 5);
+        Action::getInstance()->addAction('cms_admin_head', [$this, 'enqueueCss'], 5);
+        Action::getInstance()->addAction('cms_head', [$this, 'enqueueCss'], 5);
+    }
+
+    /**
+     * @return void
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws InvalidArgumentException
+     * @throws Exception
+     * @throws ReflectionException
+     */
+    public function setLocale(): void
+    {
+        load_plugin_textdomain(domain: $this->id(), pluginRelPath: $this->path() . 'locale');
+    }
+
+    /**
+     * @throws ReflectionException
+     * @throws Exception
+     */
+    public function enqueueCss(): void
+    {
+        cms_enqueue_css(
+            config: 'plugin',
+            asset: $this->url() . '/assets/css/style.css',
+            minify: false,
+            pluginSlug: $this->id()
+        );
+    }
+
+    /**
+     * @throws ViewException
+     * @throws InvalidTemplateNameException
+     * @throws ReflectionException
+     * @throws Exception
+     */
+    public function render(): void
+    {
+        echo $this->view->render('plugin::AdminBar/view/index', ['plugin' => $this->meta()]);
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+
+# Admin Bar
+Adds a backend admin bar to a Devflow site.
+
+> __Requires__ Devflow Version: 1.0.0
+
+> __Tested Up To:__ 1.0.0
+
+> __Requires PHP:__ 8.3+
+
+> __Stable Tag:__ 1.0.0
+
+> __License:__ GPLv2-only
+
+## Features
+- Adds admin bar to top of site.
+- Management Tab (site, plugins, users, content)
+- Flush Cache
+- Action hooks and filters.
+
+## Localization
+* English
+* Spanish
+
+## Composer Installation (Recommended for Devflow version 1.1.0 +)
+1. Start a new shell session.
+2. In the root of your install, run the following command ```composer require getdevflow/adminbar```.
+
+## Git Clone Installation (Recommended for Devflow version 1.0.0)
+1. Start a shell session and navigate to ```public/plugins``` folder.
+2. Run the following command: ```git clone https://github.com/getdevflow/AdminBar.git AdminBar```.
+
+## Changelog
+
+### 1.0.0
+- Initial addition
